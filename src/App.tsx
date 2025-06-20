d02c02a68a2f1c4b2a51f95788a1d2c7270d620d
Advit Dixit <advit1221@example.com>
2025-06-20 14:08:06 +0530

    Fix: App routes, Tailwind styles, Vercel routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React from "react";
+// src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// Import pages
+import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import SubmitFeedback from "./pages/SubmitFeedback";
-import SubmitIssue from "./pages/SubmitIssue";
-import SchoolLogin from "./pages/SchoolLogin";
 import SchoolRegister from "./pages/SchoolRegister";
+import SchoolLogin from "./pages/SchoolLogin";
 import SchoolDashboard from "./pages/SchoolDashboard";
 import FeedbackPublic from "./pages/FeedbackPublic";
 import NotFound from "./pages/NotFound";
@@ -16,12 +14,12 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/submit-feedback" element={<SubmitFeedback />} />
-        <Route path="/submit-issue" element={<SubmitIssue />} />
-        <Route path="/school-login" element={<SchoolLogin />} />
         <Route path="/register-school" element={<SchoolRegister />} />
+        <Route path="/school-login" element={<SchoolLogin />} />
         <Route path="/school-dashboard" element={<SchoolDashboard />} />
         <Route path="/feedback" element={<FeedbackPublic />} />
         <Route path="*" element={<NotFound />} />
