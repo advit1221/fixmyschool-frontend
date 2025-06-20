733ef01bd5450774941ba173284c51f23f74dc13
Advit Dixit <advit1221@example.com>
2025-06-20 14:51:02 +0530

    Fix: Full routing setup for Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Feedback from "./pages/Feedback";
-// Add your other pages here
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
 
 function App() {
   return (
@@ -10,11 +9,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/feedback" element={<Feedback />} />
-        {/* Add more routes as needed */}
+        <Route path="/contact" element={<Contact />} />
+        {/* Add more routes here */}
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
